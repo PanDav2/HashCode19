2b57b95ab9a4b0eab4de86957348bd16aee9399c
Previsou <maxime.lubin@centraliens.net>
2019-02-28 19:36:14 +0100

    score fixup

--- score.py
+++ score.py
@@ -7,31 +7,13 @@ def tags(slide):
     p1, p2 = slide
     return p1.tags.union(p2.tags)
 
+def score(transition):
+    p1, p2 = transition
+    p1tags, p2tags = tags(p1), tags(p2)
+    intersection = p1tags.intersection(p2tags)
+    sleft = len(p1tags - intersection)
+    sright = len(p2tags- intersection)
+    return min(sleft, sright, len(intersection))
 
-def left_score(l, r):
-    return len(tags(l)) - middle_score(l, r)
-
-
-def right_score(l, r):
-    return len(tags(r)) - middle_score(l, r)
-
-
-def middle_score(l, r):
-    return len(tags(l).intersection(tags(r)))
-
-
-def pairwise(iterable):
-    a, b = itertools.tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
-
-def score(slider):
-    for l, r in pairwise(slider):
-        ls = left_score(l, r)
-        ms = middle_score(l, r)
-        rs = right_score(l, r)
-
-    return min(ls, ms, rs)
-
-
+def totalscore(slides):
+    return sum(score(t) for t in zip(slides, slides[1:]))
