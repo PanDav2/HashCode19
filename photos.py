279991a0135c14972a79bfa04e504e24eb1a1a09
PandaV2 <david.panou@gmail.com>
2019-02-28 19:03:55 +0100

    class photo

---
+++ photos.py
@@ -0,0 +1,13 @@
+class Photo(object):
+	def __init__(self, id, orient, tags):
+		self.id = id
+		self.orient = orient
+		self.tags = set(tags)
+
+class Slider(object):
+	def __init__(self):
+		pass
+
+
+if __name__ == '__main__':
+	pass
